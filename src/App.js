23b315f438c2a9fe17065fecf420defea5a3d2b6
Abdelrahman Waleed <abdelrahmanmoali@gmail.com.com>
2023-03-07 10:55:43 +0200

    second action

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container id="main-container" className="h-100">
+      <Form id="asi-form" className="text-center fs-3 w-100">
+        <h1>Please Sign In</h1>
+        <Form.Group>
+          <Form.Control
+            type="email"
+            size="large"
+            placeholder="Email address"
+            autoComplete="username"
+            className="mb-3"
+          ></Form.Control>
+          <Form.Control
+            type="password"
+            size="large"
+            placeholder="password"
+            autoComplete="username"
+            className="mb-3"
+          ></Form.Control>
+          <Button className="m-2">Login</Button>
+          <Button className="m-2">Register</Button>
+        </Form.Group>
+      </Form>
+    </Container>
   );
 }
 
